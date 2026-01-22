628f91490b7a0123b410837617c121a1cf998059
Sree Raj <sreerajaravanchal@gmail.com>
2026-01-22 09:43:18 +0530

    Initial Next.js project

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="max-w-xl mx-auto mt-10 bg-white p-6 rounded shadow">
+      <h1 className="text-3xl font-bold mb-4">
+        About Page
+      </h1>
+
+      <p className="text-gray-700 leading-relaxed">
+        This page is styled using pure Tailwind CSS.
+        We are learning Next.js step by step from basics.
+      </p>
+    </div>
+  );
+}
